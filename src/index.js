c74be83eeb8ec7669dc23a18adf9d60df2fd8275
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-05-14 21:46:12 -0700

    in_progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+import _ from 'lodash.debounce';
+
+const input = document.querySelector('#search-box');
+console.log(input.value);
 
 const DEBOUNCE_DELAY = 300;
+function fetchCountries(name) {
+    return fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name`
+    );
+}
+
+
+function onInputText(e) {
+    fetchCountries(e.target.value).then(c => c.json()).then(console.log)
+}
+
+input.addEventListener('input', _(onInputText, DEBOUNCE_DELAY));
