98f2d3981bf1787ae2325dde353b05582c4a145a
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-05-15 12:00:10 -0700

    in_progress

--- src/index.js
+++ src/index.js
@@ -7,13 +7,30 @@ console.log(input.value);
 const DEBOUNCE_DELAY = 300;
 function fetchCountries(name) {
     return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name`
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
     );
 }
 
 
 function onInputText(e) {
-    fetchCountries(e.target.value).then(c => c.json()).then(console.log)
+  fetchCountries(e.target.value)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(console.log);
+}
+
+function renderCountriesList(countries) {
+  const markup = countries.map((country) => {
+  //  return `<li>
+  //         <p><b>Name</b>: ${}</p>
+  //         <p><b>Population</b>: ${}</p>
+  //         <p><b>Flag</b>: ${}</p>
+  //       </li>`;
+})  
 }
 
 input.addEventListener('input', _(onInputText, DEBOUNCE_DELAY));
