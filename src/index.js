9e46736f2323d997ab860f9f30903f51fa08880c
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-05-15 20:30:26 -0700

    in_progress

--- src/index.js
+++ src/index.js
@@ -1,36 +1,58 @@
 import './css/styles.css';
 import _ from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import API from './fetchCountries';
 
 const input = document.querySelector('#search-box');
-console.log(input.value);
+const countryList = document.querySelector('.country-list');
 
 const DEBOUNCE_DELAY = 300;
-function fetchCountries(name) {
-    return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    );
-}
 
 
 function onInputText(e) {
-  fetchCountries(e.target.value)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(console.log);
+  if (e.target.value.length === 0) {
+    return countryList.innerHTML = '';
+  }
+  
+  API.fetchCountries(e.target.value)
+    .then(countries => renderCountriesList(countries))
+    .catch(error => console.log(error));
 }
 
+
 function renderCountriesList(countries) {
-  const markup = countries.map((country) => {
-  //  return `<li>
-  //         <p><b>Name</b>: ${}</p>
-  //         <p><b>Population</b>: ${}</p>
-  //         <p><b>Flag</b>: ${}</p>
-  //       </li>`;
-})  
+  countryList.innerHTML = "";
+  if (countries.length > 10) {
+    return Notiflix.Notify.info(
+      `"Too many matches found. Please enter a more specific name."`
+    );
+  }
+  const markup = countries
+    .map(country => {
+      if (countries.length === 1) {
+        return `<li>
+    <div><img src="${country.flags.svg}" alt="flag" width = 50/>
+        <h1>${country.name.official}</h1></div>
+        </li>
+        <li>
+          <p>Capital: ${country.capital}</p>
+        </li>
+        <li>
+          <p>Population: ${country.population}</p>
+        </li>
+        <li>
+          <p>Languages: ${Object.values(country.languages)}</p>
+        </li>`;
+      }
+        return `<li>
+    <img src="${country.flags.svg}" alt="flag" width = 50/>
+        </li>
+        <li>
+        <h1>${country.name.official}</h1>
+        </li>`
+    })
+    .join('');
+  countryList.insertAdjacentHTML("beforeend",markup);
 }
 
 input.addEventListener('input', _(onInputText, DEBOUNCE_DELAY));
